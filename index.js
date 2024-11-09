79e9ab454588e0161b0fba5eaee3066fdb6cf3f4
AlejandroDiaz16 <sergiobustos69@gmail.com>
2024-11-09 16:34:56 -0500

    main app

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const taskController = require('./tasks/controller.js')
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(
+    cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,DELETE,PUT",
+    allowedHeaders: "Content-type"
+    })
+)
+app.use(express.json())
+
+// Routes
+app.use('/tasks', taskController);
+
+
+app.listen(PORT, () => console.log(`listing in port ${PORT}`))
